95d4ae225cf6a32a44021ecfb669ef2fdb115097
Andrii Taranenko <megathrone3333@gmail.com>
2024-11-27 02:59:42 +0100

    Add og

--- src/app/[locale]/metadata.ts
+++ src/app/[locale]/metadata.ts
@@ -1,9 +1,23 @@
 import type { Metadata } from "next";
 
+const imageURL: string = "/images/og_img.jpg";
+const publicURL: string = "https://udlabacu.com";
+const description: string =
+  "Tradiční česká restaurace v centru prahy. Restaurace s klasickou českou kuchyní";
+const title: string = "U Dlabačů | Tradiční česká restaurace v centru prahy";
+
 const metadata: Metadata = {
-  description:
-    "Tradiční česká restaurace v centru prahy. Restaurace s klasickou českou kuchyní",
-  title: "U Dlabačů | Tradiční česká restaurace v centru prahy",
+  description,
+  metadataBase: new URL(publicURL),
+  openGraph: {
+    description,
+    images: `${publicURL}${imageURL}`,
+    siteName: "U Dlabačů",
+    title,
+    type: "website",
+    url: publicURL,
+  },
+  title,
 };
 
 export { metadata };
